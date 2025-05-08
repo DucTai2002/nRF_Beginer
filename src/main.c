1154b44ebbe8ff33ca8fb550b38769d5b4da1db9
DucTai2002 <taichammo.vn@gmail.com>
2025-05-08 12:59:36 +0700

    lesson3 ex2: Customizing the application

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
+#include "myfunction.h"
 int main(void)
 {
- while (1) {
- printk("Hello World \n");
+ int a = 3, b = 4;
+ while(1){
+ printk("The sum of %d and %d is %dn", a, b, sum(a,b));
  k_msleep(1000);
  }
 }
